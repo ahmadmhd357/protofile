2da1f050b3d05be7ee2267b3a1bd10d97a0439c3
AHMAD ALHARIRI <100470190+ahmadmhd357@users.noreply.github.com>
2023-02-22 16:24:37 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,10 +21,10 @@ function index() {
   return (
     <div className="flex flex-col  h-screen  overflow-x-hidden overflow-y-auto scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-gray-400-green-300 dark:scrollbar-thumb-stone-500 dark:scrollbar-track-stone-300 scrollbar-thumb-rounded-full scrollbar-track-rounded-full  p-4 pt-1 ">
       <h5 className="my-5 font-bold">
-        Frontend Web developer. A hard worker with a higher purpose. I have a
-        great sense of insistence and achievement. I am a committed person who
-        always seeks to prevail and gain both knowledge and experience in the
-        field of Web development
+        Front-end web developer who works as a machine that turns coffee into code. A hard worker with a
+        higher purpose. I have a great sense of perseverance and achievement. I am a dedicated person who
+        always tries to push through and gain both knowledge and experience in the field of web
+        development.
       </h5>
       <div className="px-4 mt-6 flex-grow bg-gray-400 dark:bg-stone-800 mx-[-1.25rem]">
         <h2 className="py-3 my-3 text-xl font-bold tracking-wide">
