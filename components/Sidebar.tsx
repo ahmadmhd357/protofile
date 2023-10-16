55306bbcc401b9cb33314ea7438410c3cdce16d8
AHMAD ALHARIRI <100470190+ahmadmhd357@users.noreply.github.com>
2023-10-16 19:15:58 +0300

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -68,7 +68,7 @@ function sidebar() {
         onClick={changeTheme}
         className="w-8/12 hover:scale-110 ease-in duration-500 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400"
       >
-        Light UI
+        {theme == "light"?"Dark mode":"Light mode" }
       </button>
     </div>
   );
